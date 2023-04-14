3a014393e8a977c561ba57dbc1438a74e31693b7
hsynhsyn77 <108232382+hsynhsyn77@users.noreply.github.com>
2023-04-14 20:20:46 +0300

    Add files via upload

---
+++ src/stringDemo.java
@@ -0,0 +1,34 @@
+public class stringDemo {
+    public static void main(String[] args) {
+        String mesaj = "Bugün hava çok güzel.";
+        System.out.println(mesaj);
+
+//        System.out.println("Eleman sayısı:" + mesaj.length());
+//        System.out.println("5.eleman:" + mesaj.charAt(4));//belirli bir sırada ki karakteri buluruz.
+//        System.out.println(mesaj.concat(" Yaşasın!"));
+//        System.out.println(mesaj.startsWith("A"));//büyük küçük harf duyarlı true false döndürür
+//        System.out.println(mesaj.endsWith("."));
+//        char[] karakterler=new char[5];
+//        mesaj.getChars(0,5,karakterler,0);
+//        System.out.println(karakterler);
+//        System.out.println(mesaj.indexOf("av"));
+//        System.out.println(mesaj.lastIndexOf("a"));
+
+        String yeniMesaj = mesaj.replace(' ', '-');
+        System.out.println(yeniMesaj);
+        System.out.println(mesaj.substring(2, 5));//5 i dahil etmez.
+        System.out.println("--------------------");
+        for (String kelime:mesaj.split(" ")) {
+            System.out.println(kelime);
+        }
+        System.out.println("--------------------");
+        System.out.println(mesaj.toLowerCase());//bütün kelimeleri küçük yapar.
+        System.out.println(mesaj.toUpperCase());// bütün kelimerli byük harf yapar.
+        System.out.println("--------------------");
+        System.out.println(mesaj.trim());
+
+
+
+
+    }
+}
