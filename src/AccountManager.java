e3f9107b011c49fe30a6cff321b33076499e1fa3
hsynhsyn77 <108232382+hsynhsyn77@users.noreply.github.com>
2023-04-15 22:40:29 +0300

    Add files via upload

---
+++ src/AccountManager.java
@@ -0,0 +1,20 @@
+package throwDemo;
+
+public class AccountManager {
+    private double balance;
+
+    public void deposit(double amount){
+        balance = getBalance() + amount;
+    }
+    public void withdraw(double amount) throws BalanceInsufficentException {//hesaptan para çekme
+        if (balance>=amount) {
+            balance = getBalance() - amount;
+        }else {
+            throw new BalanceInsufficentException("Bakiye yetersiz.");
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
